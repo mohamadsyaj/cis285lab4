ee712b8e5086fd98fd1ab08227e66574ba5ed140
Hussein Chamas <hchamas@umich.edu>
2024-02-09 12:08:05 -0500

    added name and changed iteration

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        for(int i=1;i<=12;i++) {
+        for(int i=1;i<=11;i++) {
                 System.out.println("Hello World from Moe and Moe and Hussein"+i);
         }
     }
